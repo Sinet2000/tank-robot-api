df36153d194a36a854b8c7575b6ef64eb5413aae
Sinet2000 <giinger11352@gmail.com>
2023-04-22 22:27:46 +0300

    Updated configuration

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# Initialize environment and install required packages
-python3.9 -m venv venv
-source venv/bin/activate
 
 # Setup conda env
-# Install Miniconda (replace Miniconda3-latest-Linux-x86_64.sh with the appropriate file for your system)
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $PWD/miniconda
+# Check if conda is installed
+if ! command -v conda &> /dev/null
+then
+    # Download and install Miniconda (replace Miniconda3-latest-Linux-x86_64.sh with the appropriate file for your system)
+    # Install Miniconda (replace Miniconda3-latest-Linux-x86_64.sh with the appropriate file for your system)
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+    bash ~/miniconda.sh -b -p "${PWD}"/miniconda
 
-# Activate the conda environment
-source $PWD/miniconda/bin/activate
+    # Activate the conda environment
+    source "${PWD}"/miniconda/bin/activate
+fi
 
 # Create a new environment called "flask-env" and install Flask
-conda create -n tank-robot-api-env flask
+conda create -n tank-robot-api-env flask python=3.9
 
 # Activate the "flask-env" environment
 conda activate tank-robot-api-env
@@ -23,15 +25,15 @@ pip install -r requirements.tx
 yolov5_path=$(pwd)/neural_networks/yolov5
 
 # Check if the yolov5 folder exists
-if [ ! -d "$yolov5_path" ]; then
+if [[ ! -d "${yolov5_path}" ]]; then
     # Clone the yolov5 repository from GitHub
-    git clone https://github.com/ultralytics/yolov5 "$yolov5_path"
-    cd $yolov5_path
+    git clone https://github.com/ultralytics/yolov5 "${yolov5_path}"
+    cd "${yolov5_path}" || exit
     git reset --hard fbe67e465375231474a2ad80a4389efc77ecff99
 fi
 
 # Install the required packages
-pip install -qr "$yolov5_path/requirements.txt"
+pip install -qr "${yolov5_path}/requirements.txt"
 
 pip install torch==1.8.0
 pip install torchvision==0.9.0
