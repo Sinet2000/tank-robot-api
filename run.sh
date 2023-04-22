4995abb842ea4f2ac1272dd65fedf8850199df39
Sinet2000 <giinger11352@gmail.com>
2023-04-22 13:44:58 +0300

    Added inital setup scripts and required packages in requirements.txt

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Initialize environment and install required packages
+python3.9 -m venv venv
+source venv/bin/activate
+pip install -r requirements.tx
 
 # Define the path to the yolov5 folder
 yolov5_path=$(pwd)/neural_networks/yolov5
@@ -7,10 +11,16 @@ yolov5_path=$(pwd)/neural_networks/yolov5
 if [ ! -d "$yolov5_path" ]; then
     # Clone the yolov5 repository from GitHub
     git clone https://github.com/ultralytics/yolov5 "$yolov5_path"
+    cd $yolov5_path
+    git reset --hard fbe67e465375231474a2ad80a4389efc77ecff99
 fi
 
-# Install the required packages using pip
-pip install -r "$yolov5_path/requirements.txt"
+# Install the required packages
+pip install -qr "$yolov5_path/requirements.txt"
 
+pip install torch==1.8.0
+pip install torchvision==0.9.0
+
+cd ..
 # Run the app
 python app.py
